89b1b861e04a0664ca9cc362dbbd6298b04c9d35
Dhruv Singhania <dhruv.s0611@gmail.com>
2026-02-23 23:28:21 +0530

    First Commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Hello World" });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
