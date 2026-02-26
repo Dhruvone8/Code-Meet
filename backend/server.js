bb001983eb6156b9238484f62eb8cc02f1d765bf
Dhruv Singhania <dhruv.s0611@gmail.com>
2026-02-26 08:45:00 +0530

    auth and db sync

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import cors from "cors";
+import { serve } from "inngest/express";
+import { inngest, inngestFunctions } from "./utils/inngest.js";
 dotenv.config({quiet: true});
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Middlewares
+app.use(express.json());
+app.use(cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true
+}));
+
+// Routes
+app.use('/inngest', serve({client: inngest, functions: []}))
 
 app.get("/", (req, res) => {
     res.status(200).json({ message: "Hello World" });
